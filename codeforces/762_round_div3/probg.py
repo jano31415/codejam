8567d51451e7f38f0c9d945e14cce70a749742d2
jano31415 <jp.eisenbach@gmail.com>
2021-12-23 12:41:19 +0100

    implementing first idea on g, but only works fork k=0,k=1

--- codeforces/762_round_div3/probg.py
+++ codeforces/762_round_div3/probg.py
@@ -4,64 +4,45 @@ from heapq import heapify,heappush,heappop
 
 def solve(n, k, mines):
     if k==0:
-        return solve0(n,mines)
+        times = [x[2] for x in mines]
+        return solve0(times)
     else:
+        # this works only for k=1
+        # k can be up to 10**9 which makes this infeasible
         return solve1(n,mines)
 
-def solve0(n,mines):
-    times = [x[2] for x in mines]
+def solve0(times):
+
     times.sort(reverse=True)
-    for i in range(n):
+    for i in range(len(times)):
         if times[i] < i:
             return i-1
-    return n
+    return len(times)-1
 
 def solve1(n, mines):
-    pass
-
-# connected component algorithm, maybe reuse some of the spanning tree algorithm
-
-def kruskal(edges, nodes):
-    edges.sort(key=lambda x:-x[0])
-    span_tree = []
-    node_to_rank = {}
-    rank_to_points = {0:[]}
-    max_rank = -1
-    for w,a,b in edges:
-        max_rank = add_edge(w,a,b,node_to_rank, rank_to_points,max_rank, span_tree)
-        if len(span_tree) == len(nodes) -1:
-            break
-    return span_tree
-
-
-def add_edge(w,a,b, node_to_rank, rank_to_points, max_rank, span_tree):
-    if a in node_to_rank:
-        if b not in node_to_rank:
-            node_to_rank[b] = node_to_rank[a]
-            rank_to_points[node_to_rank[a]].append(b)
-            span_tree.append((w, a, b))
-        else:
-            if node_to_rank[a] != node_to_rank[b]:
-                max_rank_ab = max(node_to_rank[a], node_to_rank[b])
-                switch_points = rank_to_points[max_rank_ab]
-                new_rank = min(node_to_rank[a],node_to_rank[b])
-                for x in switch_points:
-                    node_to_rank[x] = new_rank
-                    rank_to_points[new_rank].append(x)
-                del rank_to_points[max_rank_ab]
-                span_tree.append((w,a,b))
-            else:
-                pass  # cycle dont add
-        return max_rank
-    if b in node_to_rank:
-        return add_edge(w,b,a, node_to_rank, rank_to_points,max_rank, span_tree)
-    new_max_rank = max_rank + 1
-    node_to_rank[a] = new_max_rank
-    node_to_rank[b] = new_max_rank
-    rank_to_points[new_max_rank] = [a, b]
-    span_tree.append((w, a, b))
-    return new_max_rank
-
+    mines_locations = {(mx,my):t for mx,my,t in mines}
+    mines_left = set(mines_locations.keys())
+    components = []
+    while len(mines_left) > 0:
+        start_mine = mines_left.pop()
+        #bfs
+        q = [start_mine]
+        seen = set(q)
+        while len(q) > 0:
+            cur = q.pop()
+            try:
+                mines_left.remove(cur)
+            except Exception:
+                pass
+            for x,y in [(-1,0), (1,0), (0,-1), (0,1)]:
+                next_node = (cur[0]+x, cur[1]+y)
+                if next_node in mines_locations:
+                    if next_node not in seen:
+                        q.append(next_node)
+                        seen.add(next_node)
+        components.append(seen)
+    comp_time = [min([mines_locations[x] for x in comp]) for comp in components]
+    return solve0(comp_time)
 
 
 import os
